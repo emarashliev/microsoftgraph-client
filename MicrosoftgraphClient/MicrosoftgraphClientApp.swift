c95ee3f414c3f0f0dcf81a83306ad2fcf3275127
Emil Marashliev <emarashliev@vmware.com>
2024-06-29 00:48:22 +0300

    First commit

---
+++ MicrosoftgraphClient/MicrosoftgraphClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicrosoftgraphClientApp.swift
+//  MicrosoftgraphClient
+//
+//  Created by Emil Marashliev on 29.06.24.
+//
+
+import SwiftUI
+
+@main
+struct MicrosoftgraphClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
